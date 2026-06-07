ad5ea1260e4053f1d98b0a66567a71b24239b1ee
Alex Parsons <alex.parsons@mysociety.org>
2026-06-07 11:43:36 +0000

    Add query approach

--- src/pydantic_store/__init__.py
+++ src/pydantic_store/__init__.py
@@ -4,7 +4,7 @@ Helper models and backports
 
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "BaseModel",
@@ -13,7 +13,9 @@ __all__ = [
     "DictModel",
     "JsonStore",
     "PydanticDBM",
+    "is_in",
 ]
 
 from .dbm import PydanticDBM
 from .models import BaseModel, DictModel, JsonStore, ListModel, RootModel
+from .query import is_in
